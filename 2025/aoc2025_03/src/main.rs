63906e5fd3679ad715e1edfc909a9dad68de2ee1
tylp <antoine.plard.dev@proton.me>
2025-12-06 18:56:13 +0100

    Ugly solve 2025d3p1

--- 2025/aoc2025_03/src/main.rs
+++ 2025/aoc2025_03/src/main.rs
@@ -1,31 +1,13 @@
-type Joltage = u8;
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-enum BatteryState {
-    On,
-    Off,
-}
+type Joltage = u32;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct Battery {
     joltage: Joltage,
-    state: BatteryState,
 }
 
 impl Battery {
     pub fn new(joltage: Joltage) -> Self {
-        Self {
-            joltage,
-            state: BatteryState::Off,
-        }
-    }
-
-    pub fn switch_on(&mut self) {
-        self.state = BatteryState::On
-    }
-
-    pub fn switch_off(&mut self) {
-        self.state = BatteryState::Off
+        Self { joltage }
     }
 }
 
@@ -39,37 +21,40 @@ impl BatteryBank {
         Self { batteries }
     }
 
-    pub fn largest_possible_joltage(&self) -> u8 {
-        // We do not want to move battery ordering so we clone it
-        let mut batteries = self.batteries.clone();
+    /// Find the largest possible voltage for the bank by finding the two
+    /// highest values with preserved order.
+    pub fn largest_possible_two_digit_joltage(&self) -> u32 {
+        let mut best = 0u32;
+        let len = self.batteries.len();
 
-        batteries.sort_by_key(|battery| battery.joltage);
+        let digits: Vec<u32> = self.batteries.iter().map(|b| b.joltage).collect();
 
-        let first = batteries.first();
-        let second = batteries.get(1);
-
-        match (first, second) {
-            (None, None) => 0,
-            (None, Some(second)) => second.joltage,
-            (Some(first), None) => first.joltage,
-            (Some(first), Some(second)) => format!("{}{}", first.joltage, second.joltage)
-                .parse::<u8>()
-                .unwrap_or_else(|_| {
-                    panic!(
-                        "Failed to parse first and second: {}, {}",
-                        first.joltage, second.joltage
-                    )
-                }),
+        for i in 0..len {
+            for j in (i + 1)..len {
+                let val = digits[i] * 10 + digits[j];
+                if val > best {
+                    best = val;
+                }
+            }
         }
+
+        best
+    }
+
+    /// Return the largest possible joltage for the bank by turning on
+    /// a given number of digits.
+    pub fn larget_possible_joltage_for_digits(&self, digits: u32) -> u32 {
+        todo!()
     }
 }
 
 impl From<&str> for BatteryBank {
     fn from(value: &str) -> Self {
         let batteries: Vec<Battery> = value
-            .as_bytes()
-            .iter()
-            .map(|joltage| Battery::new(*joltage))
+            .chars()
+            .map(|c| c.to_digit(10))
+            .filter(|item| item.is_some())
+            .map(|joltage| Battery::new(joltage.unwrap()))
             .collect();
         BatteryBank::new(batteries)
     }
@@ -82,11 +67,11 @@ fn main() {
         .map(|line| BatteryBank::from(line.as_str()))
         .collect();
 
-    println!("Battery banks: {:?}", banks);
-}
+    let total_output = banks
+        .iter()
+        .fold(0, |acc, e| acc + e.largest_possible_two_digit_joltage());
 
-fn solve() -> u64 {
-    unimplemented!()
+    println!("Total output of the banks: {total_output}");
 }
 
 #[cfg(test)]
@@ -95,9 +80,16 @@ mod tests {
 
     #[test]
     fn test_example() {
-        let input = "987654321111111";
-        let bank = BatteryBank::from(input);
+        let bank = BatteryBank::from("987654321111111");
+        assert_eq!(bank.largest_possible_two_digit_joltage(), 98);
 
-        assert_eq!(bank.largest_possible_joltage(), 98);
+        let bank = BatteryBank::from("811111111111119");
+        assert_eq!(bank.largest_possible_two_digit_joltage(), 89);
+
+        let bank = BatteryBank::from("234234234234278");
+        assert_eq!(bank.largest_possible_two_digit_joltage(), 78);
+
+        let bank = BatteryBank::from("818181911112111");
+        assert_eq!(bank.largest_possible_two_digit_joltage(), 92);
     }
 }
