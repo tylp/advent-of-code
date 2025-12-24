27bd01e26c8f0aaa9901e21da71b89da38258852
tylp <antoine.plard.dev@proton.me>
2025-12-24 11:47:10 +0100

    Implement 2025d5p1.

--- 2025/aoc2025_05/src/main.rs
+++ 2025/aoc2025_05/src/main.rs
@@ -1,59 +1,85 @@
-#[derive(Debug)]
-struct Recipe {
-    fresh_ingredients_range: Vec<Vec<u64>>,
-    available_ingredients: Vec<u64>,
+fn main() {
+    let lines = aoc::init();
+
+    // Find the adrress of the
+    let mut separator = lines.split(|line| line.is_empty());
+    let ranges = separator.next().expect("Failed to get ranges");
+    let ingredients = separator.next().expect("Failed to get ingredients");
+
+    let recipe = Recipe {
+        ingredients,
+        ranges,
+    };
+
+    let number_of_fresh_ingredient_ids = recipe.count_fresh_ingredients_ids();
+    println!("Number of fresh ingredient ids is {number_of_fresh_ingredient_ids}");
 }
 
-impl Recipe {
-    pub fn new(lines: Vec<String>) -> Self {
-        let (index, _blank) = lines
-            .iter()
-            .enumerate()
-            .find(|line| line.1.is_empty())
-            .expect("The recipe is malformed");
+struct Recipe<'a> {
+    ranges: &'a [String],
+    ingredients: &'a [String],
+}
 
-        let (left, right) = lines.split_at(index);
+impl<'a> Recipe<'a> {
+    fn count_fresh_ingredients_ids(&self) -> usize {
+        let mut fresh_ingredients = 0u64;
 
-        let fresh_ingredients_range = left
-            .iter()
-            .map(|line| {
-                let parts: Vec<_> = line.split("-").collect();
-
-                let start = parts
+        for ingredient in self.ingredients {
+            for range in self.ranges {
+                let ranges: Vec<&str> = range.split("-").collect();
+                let range_start = ranges
                     .first()
-                    .expect("Failed to get start of range")
+                    .expect("Failed to get range start")
                     .parse::<u64>()
-                    .expect("Failed to parse start of range");
-                let end = parts
+                    .expect("Failed to parse range start");
+                let range_end = ranges
                     .last()
-                    .expect("Failed to get end of range")
+                    .expect("Failed to get range end")
                     .parse::<u64>()
-                    .expect("Failed to parse end of range");
+                    .expect("Failed to parse range end");
 
-                (start..=end).collect()
-            })
-            .collect();
+                let ingredient = ingredient
+                    .parse::<u64>()
+                    .expect("Failed to parse ingredient id");
 
-        let available_ingredients = right
-            .iter()
-            .map(|line| {
-                line.parse::<u64>()
-                    .expect("Failed to parse available ingredient")
-            })
-            .collect();
-
-        Self {
-            fresh_ingredients_range,
-            available_ingredients,
+                if ingredient >= range_start && ingredient <= range_end {
+                    fresh_ingredients += 1;
+                    break;
+                }
+            }
         }
+
+        fresh_ingredients as usize
     }
 }
 
-fn main() {
-    let recipe = Recipe::new(aoc::init());
-
-    println!("Built a new recipe: {recipe:?}");
-}
-
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::Recipe;
+
+    /// Test the part 1 example with the following input :
+    /// 3-5
+    /// 10-14
+    /// 16-20
+    /// 12-18
+    ///
+    /// 1
+    /// 5
+    /// 8
+    /// 11
+    /// 17
+    /// 32
+    #[test]
+    fn count_fresh_ingredients_ids_test() {
+        let ranges = ["3-5", "10-14", "16-20", "12-18"].map(str::to_string);
+        let ingredients = ["1", "5", "8", "11", "17", "32"].map(str::to_string);
+
+        let recipe = Recipe {
+            ranges: &ranges,
+            ingredients: &ingredients,
+        };
+
+        let fresh = recipe.count_fresh_ingredients_ids();
+        assert_eq!(fresh, 3);
+    }
+}
